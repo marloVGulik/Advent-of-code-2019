fde615de756c44f96aa5ce0d2eec91ff695c019a
Marlo van Gulik <mjvangulik208@gmail.com>
2019-12-12 17:01:05 +0100

    Day 3 done

--- Advent of code/Advent of code.cpp
+++ Advent of code/Advent of code.cpp
@@ -8,12 +8,17 @@
 
 int main()
 {
-	std::ifstream inputFile("input.txt");
-	std::ofstream outputFile("output.txt");
-	std::ifstream inputOfCodes("inputOfCodes.txt");
-	std::ofstream outputOfCodes("outputOfCodes.txt");
-	if (!inputFile.is_open() || !inputOfCodes.is_open() || !outputFile.is_open()) {
-		return -1;
+	// Day one in/out
+	std::ifstream inputFile("data/input/input.txt");
+	std::ofstream outputFile("data/output/output.txt");
+
+	// Day two in/out
+	std::ifstream inputOfCodes("data/input/inputOfCodes.txt");
+	std::ofstream outputOfCodes("data/output/outputOfCodes.txt");
+
+	// Day three in/out
+	if (!inputFile.is_open() || !inputOfCodes.is_open() || !outputFile.is_open() || !outputOfCodes.is_open()) {
+		throw std::runtime_error("ERROR: FILES DIDN'T OPEN!");
 	}
 
 
@@ -27,11 +32,11 @@ int main()
 		calcOutput = std::stoi(textInputFuel);
 		calcOutput /= 3;
 		calcOutput -= 2;
-		std::cout << calcOutput << std::endl;
+		//std::cout << calcOutput << std::endl;
 
 		int additionalFuelPerModule = calcOutput / 3 - 2;
 		do {
-			std::cout << additionalFuelPerModule << std::endl;
+			//std::cout << additionalFuelPerModule << std::endl;
 			additionalFuelForFuel += additionalFuelPerModule;
 			additionalFuelPerModule /= 3;
 			additionalFuelPerModule -= 2;
@@ -41,86 +46,92 @@ int main()
 		lineNumber++;
 	}
 
-	std::cout << additionalFuelForFuel << " + " << totalOutput << " = " << std::endl;
-	std::cout << totalOutput + additionalFuelForFuel << std::endl;
+	std::cout << additionalFuelForFuel << " + " << totalOutput << " = " << totalOutput + additionalFuelForFuel << std::endl;
+	//std::cout << totalOutput + additionalFuelForFuel << std::endl;
 	outputFile << totalOutput + additionalFuelForFuel << "\n";
 
-
-	// 
+	std::cout << std::endl;
+	// Day 2
+	int defaultActionArray[MAX_STRING];
 	std::string textInputOfCodes;
 	int lineNumberOfNumbers = 0;
-	int currentActionArray[MAX_STRING];
 	while (std::getline(inputOfCodes, textInputOfCodes)) {
 		std::stringstream ofCodesLineStream(textInputOfCodes);
 		std::string textOfInputCodesSmallText;
 
 		while (std::getline(ofCodesLineStream, textOfInputCodesSmallText, ',')) {
-			currentActionArray[lineNumberOfNumbers] = std::stoi(textOfInputCodesSmallText);
+			defaultActionArray[lineNumberOfNumbers] = std::stoi(textOfInputCodesSmallText);
 			lineNumberOfNumbers++;
 		}
 	}
-	//std::cout << lineNumberOfNumbers << ": lineNumberOfNumbers" << std::endl;
-	//for (int i = 0; i < MAX_STRING; i += 4) {
-	//	if (currentActionArray[i] == 1) {
-	//		currentActionArray[i + 3] = currentActionArray[currentActionArray[i + 1]] + currentActionArray[currentActionArray[i + 2]];
-	//		std::cout << "1: Stored: " << currentActionArray[i + 3] << " at " << i + 3 << std::endl;
-	//	}
-	//	else if (currentActionArray[i] == 2) {
-	//		int multiplication = currentActionArray[currentActionArray[i + 1]] * currentActionArray[currentActionArray[i + 2]];/*
-	//		std::cout << currentActionArray[currentActionArray[i + 1]] << " * " << currentActionArray[currentActionArray[i + 2]] << " = " << multiplication << std::endl;
-	//		std::cout << currentActionArray[i + 1] << " * " << currentActionArray[i + 2] << " = " << currentActionArray[i + 3] << std::endl;*/
-	//		currentActionArray[currentActionArray[i + 3]] = multiplication;
-	//		if (currentActionArray[i + 3] < MAX_STRING) {
-	//			std::cout << "2: Stored: " << currentActionArray[currentActionArray[i + 3]] << " at " << currentActionArray[i + 3] << std::endl;
-	//		}
-	//		else {
-	//			std::cout << "2: Didn't store at: " << currentActionArray[i + 3] << std::endl;
-	//		}
-	//	}
-	//	else if (currentActionArray[i] == 99) {
-	//		std::cout << "99 found at line " << i << std::endl;
-	//		break;
-	//	}
-	//	else {
-	//		std::cout << "ERROR" << std::endl;
-	//		return -1;
-	//	}
-	//}
-	for (int i = 0; i < lineNumberOfNumbers; i += 4) {
-		int opCode = currentActionArray[i];
-		int calcOne = currentActionArray[i + 1];
-		int calcTwo = currentActionArray[i + 2];
-		int* answerLoc = &currentActionArray[i + 3];
-		if (opCode == 1) {
-			*answerLoc = calcOne + calcTwo;
-			std::cout << "1: " << calcOne << " + " << calcTwo << " = " << *answerLoc << std::endl;
-		}
-		else if (opCode == 2) {
-			*answerLoc = calcOne * calcTwo;
-			std::cout << "2: " << calcOne << " * " << calcTwo << " = " << *answerLoc << std::endl;
-		}
-		else {
-			std::cout << "Stopped calculating" << std::endl;
-			break;
+
+	int counter = 0;
+	for (int noun = 0; noun < 100; noun++) {
+		for (int verb = 0; verb < 100; verb++) {
+			int currentActionArray[MAX_STRING];
+			for (int i = 0; i < MAX_STRING; i++) {
+				currentActionArray[i] = defaultActionArray[i];
+			}
+
+			currentActionArray[1] = noun;
+			currentActionArray[2] = verb;
+			//std::cout << currentActionArray[1] << ", " << currentActionArray[2] << std::endl;
+
+
+			for (int i = 0; i < lineNumberOfNumbers; i += 4) {
+				int* initialValues[4] = { &currentActionArray[i], &currentActionArray[currentActionArray[i + 1]], &currentActionArray[currentActionArray[i + 2]], &currentActionArray[currentActionArray[i + 3]] };
+				int* opCode = initialValues[0];
+				int* calcOne = initialValues[1];
+				int* calcTwo = initialValues[2];
+				int* answerLoc = &currentActionArray[currentActionArray[i + 3]];
+
+				// What to do with the intcodes?
+				if (*opCode == 1) {
+					*answerLoc = *calcOne + *calcTwo;
+					//std::cout << "1: " << *calcOne << " + " << *calcTwo << " = " << *answerLoc << "; Saving at: " << currentActionArray[i + 3] << std::endl;
+				}
+				else if (*opCode == 2) {
+					*answerLoc = *calcOne * *calcTwo;
+					//std::cout << "2: " << *calcOne << " * " << *calcTwo << " = " << *answerLoc << "; Saving at: " << currentActionArray[i + 3] << std::endl;
+				}
+				else {
+					//std::cout << "Stopped calculating" << std::endl;
+					i = lineNumberOfNumbers;
+				}
+			}
+			//std::cout << std::endl;
+
+			for (int i = 0; i < lineNumberOfNumbers; i += 4) {
+				if (currentActionArray[i] == 99) {
+					//outputOfCodes << "99, ";
+					//std::cout << "99, " << std::endl;
+					i -= 3;
+				}
+				else if (currentActionArray[i] != -858993460 || currentActionArray[i + 1] != -858993460 || currentActionArray[i + 2] != -858993460 || currentActionArray[i + 3] != -858993460) {
+					//std::cout << currentActionArray[i] << ", " << currentActionArray[i + 1] << ", " << currentActionArray[i + 2] << ", " << currentActionArray[i + 3] << ", " << std::endl;
+					/*outputOfCodes << currentActionArray[i] << ", ";
+					outputOfCodes << currentActionArray[i + 1] << ", ";
+					outputOfCodes << currentActionArray[i + 2] << ", ";
+					outputOfCodes << currentActionArray[i + 3] << ", ";
+					outputOfCodes << "\n";*/
+				}
+			}
+			int* answerDay2 = &currentActionArray[0];
+			if (noun == 12 && verb == 2) {
+				std::cout << *answerDay2 << std::endl;
+			}
+			if (*answerDay2 == 19690720) {
+				std::cout << "Noun: " << noun << ", verb: " << verb << ", answer: " << 100 * noun + verb << std::endl;
+				break;
+			}
+			else {
+				//std::cout << counter << ": " << currentActionArray[0] << ", " << currentActionArray[1] << ", " << currentActionArray[2] << std::endl;
+			}
+			counter++;
+			outputOfCodes << currentActionArray[0] << "\n";
 		}
 	}
 
-	for (int i = 0; i < lineNumberOfNumbers; i += 4) {
-		if (currentActionArray[0] == 99) {
-			outputOfCodes << "99, ";
-			i -= 4;
-		}
-		else if (currentActionArray[i] != -858993460 || currentActionArray[i + 1] != -858993460 || currentActionArray[i + 2] != -858993460 || currentActionArray[i + 3] != -858993460) {
-			std::cout << currentActionArray[i] << ", " << currentActionArray[i + 1] << ", " << currentActionArray[i + 2] << ", " << currentActionArray[i + 3] << ", " << std::endl;
-			outputOfCodes << currentActionArray[i] << ", ";
-			outputOfCodes << currentActionArray[i + 1] << ", ";
-			outputOfCodes << currentActionArray[i + 2] << ", ";
-			outputOfCodes << currentActionArray[i + 3] << ", ";
-			outputOfCodes << "\n";
-		}
-	}
-
-	std::cout << currentActionArray[0] << std::endl;
 
 	// MAKE NEW AND OLD ARRAY ^^
 
